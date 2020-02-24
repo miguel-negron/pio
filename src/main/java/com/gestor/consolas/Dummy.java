7f6b966271343dfaa7dbf63598a998b3e9ec5c86
Miguel <miguel.negron@gmail.com>
2020-02-24 12:49:06 +0100

    merge con lo de rober

--- src/main/java/com/gestor/consolas/Dummy.java
+++ src/main/java/com/gestor/consolas/Dummy.java
@@ -8,6 +8,10 @@ import com.gestor.clases.Alumno;
 public class Dummy {
 
 	public static void main(String[] args) {
+		//a1();
+	}
+
+	private static void a1() {
 		// TODO Auto-generated method stub
 		
 		Alumno a1 = new Alumno("1234S", "nom", "a1", "a2", LocalDate.of(2002, 5, 7), null, true, true);
